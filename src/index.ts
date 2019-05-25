6668a9285129afd809206f82edff441b799c44cb
titouancreach <titouan.creach@gmail.com>
2019-05-25 19:05:30 +0200

    ✨ Parse practical infos and the datetime from the website

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import $ from "jquery";
+import moment from "moment";
+import "moment/locale/fr";
+
+function debugString(a: string, b: string) {
+  const max = Math.min(a.length, b.length);
+
+  for (let i = 0; i < max; i++) {
+    console.log("a[i] === b[i]", a[i], b[i], a[i] === b[i]);
+  }
+}
 
 type Artist = string;
 
+type PracticalInformation = {
+  dateTime: moment.Moment;
+  price: string;
+  location: string;
+};
+
 function hasInfosPratiquesBlock(): boolean {
-  return !!$(".blockheader>h2:contains(Infos pratiques)").length;
+  return !!$(".blockheader > h2:contains(Infos pratiques)").length;
 }
 
 function getArtists(): Artist[] {
-  const groupesDiv = $(
-    "#page_content > div > div:nth-child(3) > div.eventartists > div.blockcontent > div > div > ul > li"
+  const artistsElement = $(
+    "div.eventartists > div.blockcontent > div > div > ul > li"
   );
 
-  const groupes = $(groupesDiv)
+  const groupes = $(artistsElement)
     .map(function() {
       return this.textContent;
     })
@@ -26,13 +42,54 @@ function getArtists(): Artist[] {
   return groupes;
 }
 
+function normalizeString(s: string): string {
+  return s
+    .trim()
+    .replace("\n", "")
+    .replace(/\s\s+/g, " ") // multiple spaces by 1 space
+    .replace(/\u00a0/g, " "); // remove nbsp
+}
+
+function getPracticalsInfos(): PracticalInformation {
+  const practicalInformationElement = $(
+    "div.sidebareventinfos > div.blockcontent"
+  );
+
+  const dateTime = $(practicalInformationElement)
+    .find(":contains(Date et heure)")
+    .next()
+    .text();
+
+  const price = $(practicalInformationElement)
+    .find(":contains(Prix)")
+    .next()
+    .text();
+
+  const location = $(practicalInformationElement)
+    .find(":contains(Lieu)")
+    .next()
+    .text();
+
+  const momentDateTime = moment(normalizeString(dateTime), "LLL", "fr");
+
+  return {
+    dateTime: momentDateTime,
+    price: normalizeString(price),
+    location: normalizeString(location)
+  };
+}
+
 $(document).ready(() => {
   if (!hasInfosPratiquesBlock()) {
     return;
   }
+  console.log("This page can be parsed");
 
   const artists = getArtists();
+  console.log("Artists are ", artists);
 
-  console.log("Artists are", artists);
-  console.log("This page can be parsed !!");
+  const practicalInfos = getPracticalsInfos();
+  console.log("DateTime is ", practicalInfos.dateTime.format());
+  console.log("price is ", practicalInfos.price);
+  console.log("location is", practicalInfos.location);
 });
