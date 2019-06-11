a86f2ecae85e8379ea91826fab7ac25cecfec208
titouancreach <titouan.creach@gmail.com>
2019-06-11 23:11:23 +0200

    ✨ Add the "u"

--- src/string.test.ts
+++ src/string.test.ts
@@ -5,6 +5,7 @@ test("should remove accents to a string", () => {
   expect(removeAccents("été")).toBe("ete");
   expect(removeAccents("à")).toBe("a");
   expect(removeAccents("maïs")).toBe("mais");
+  expect(removeAccents("août")).toBe("aout");
 });
 
 test("Should not transform a string if there is no accents in it", () => {
